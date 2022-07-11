414bd7024b92de9556954485832f0ca81ece13fa
Olga Shalakhina <osshalakhina@gmail.com>
2022-07-11 11:15:30 +0300

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-10">
       <div>
         <h1 className="text-5xl font-signature ml-2">OlgaShalakhina</h1>
       </div>
